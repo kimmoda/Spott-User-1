42157d6e56a3f81790ac4473bd1ad75a18daa016
funnywebdev <alexrunner82@gmail.com>
2016-06-28 11:01:23 +0200

    Added title attributes to the top productsFromMediumTile component

--- src/pages/home2/view/topSellingProducts/index.js
+++ src/pages/home2/view/topSellingProducts/index.js
@@ -7,69 +7,89 @@ import ProductsFromMediumTile from '../../../_common/tiles/productsFromMediumTil
 const dummyScenes = fromJS([ {
   image: require('./images/daredevil.jpg'),
   logo: require('./images/daredevilLogo.png'),
+  name: 'Daredevil',
   products: [ {
+    name: 'Backpack',
     id: 'XYZ1',
     image: require('./images/backpack.png')
   }, {
+    name: 'Eames',
     id: 'XYZ2',
     image: require('./images/eames.png')
 
   }, {
+    name: 'Lasagna',
     id: 'XYZ3',
     image: require('./images/lasagna.png')
 
   }, {
+    name: 'Tabasco',
     id: 'XYZ4',
     image: require('./images/tabasco.png')
   } ]
 }, {
   image: require('./images/gameOfThrones.jpg'),
   logo: require('./images/gameOfThronesLogo.png'),
+  name: 'Game of Thrones',
   products: [ {
+    name: 'Backpack',
     id: 'XYZ5',
     image: require('./images/backpack.png')
   }, {
+    name: 'Eames',
     id: 'XYZ6',
     image: require('./images/eames.png')
 
   }, {
+    name: 'Lasagna',
     id: 'XYZ7',
     image: require('./images/lasagna.png')
 
   }, {
+    name: 'Tabasco',
     id: 'XYZ8',
     image: require('./images/tabasco.png')
   } ]
 }, {
   image: require('./images/warcraft.jpg'),
   logo: require('./images/warcraftLogo.png'),
+  name: 'Warcraft',
   products: [ {
+    name: 'Backpack',
     id: 'XYZ9',
     image: require('./images/backpack.png')
   }, {
+    name: 'Eames',
     id: 'XYZ10',
     image: require('./images/eames.png')
 
   }, {
+    name: 'Lasagna',
     id: 'XYZ11',
     image: require('./images/lasagna.png')
   }, {
+    name: 'Tabasco',
     id: 'XYZ12',
     image: require('./images/tabasco.png')
   } ]
 }, {
   image: require('./images/daredevil.jpg'),
   logo: require('./images/daredevilLogo.png'),
+  name: 'Daredevil',
   products: [ {
+    name: 'Backpack',
     id: 'XYZ13',
     image: require('./images/backpack.png')
   }, {
+    name: 'Eames',
     id: 'XYZ14',
     image: require('./images/eames.png')
   }, {
+    name: 'Lasagna',
     id: 'XYZ15',
     image: require('./images/lasagna.png')
   }, {
+    name: 'Tabasco',
     id: 'XYZ16',
     image: require('./images/tabasco.png')
   } ]
