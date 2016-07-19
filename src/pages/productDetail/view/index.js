3ee145be0383a63cb90586a39ddd165e41c23add
funnywebdev <alexrunner82@gmail.com>
2016-07-19 14:16:55 +0200

    Changed button to a tag because firefox doesn't allow flex on button elements. Also added styling

--- src/pages/productDetail/view/index.js
+++ src/pages/productDetail/view/index.js
@@ -58,7 +58,8 @@ export default class ProductDetail extends Component {
     }
   }
 
-  share () {
+  share (e) {
+    e.preventDefault();
     window.open(`http://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}&title=Discover ${this.props.product.get('shortName')} now on Spott`, 'name', 'width=600,height=400');
   }
 
@@ -166,9 +167,26 @@ export default class ProductDetail extends Component {
         shareButton: {
           backgroundColor: 'transparent',
           border: 'solid 0.125em',
+          borderColor: colors.coolGray,
+          color: colors.coolGray,
+          textDecoration: 'none',
+          fill: colors.coolGray,
           borderRadius: '20em',
-          lineHeight: '1em',
-          display: 'inline-block'
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingTop: '0.188em',
+          paddingBottom: '0.188em',
+          paddingLeft: '0.625em',
+          paddingRight: '0.625em',
+          ':hover': {
+            borderColor: colors.dark,
+            color: colors.dark,
+            fill: colors.dark
+          }
+        },
+        shareText: {
+          ...makeTextStyle(fontWeights.regular, '1em')
         },
         shareIcon: {
           width: '1em',
@@ -231,13 +249,15 @@ export default class ProductDetail extends Component {
                     <Button href={product.getIn([ 'offerings', '0', 'url' ])} style={pinkButtonStyle} target='_blank'>
                       <span style={styles.details.buttons.buyText}>{t('productDetail.buyNow').toUpperCase()}</span>
                     </Button>
-                    <button
+                    <a
+                      href='#'
                       style={styles.details.buttons.shareButton}
                       onClick={this.share}>
                       <svg style={styles.details.buttons.shareIcon} viewBox='0 0 481.6 481.6' xmlns='http://www.w3.org/2000/svg'>
                         <path d='M381.6 309.4c-27.7 0-52.4 13.2-68.2 33.6l-132.3-73.9c3.1-8.9 4.8-18.5 4.8-28.4 0-10-1.7-19.5-4.9-28.5l132.2-73.8c15.7 20.5 40.5 33.8 68.3 33.8 47.4 0 86.1-38.6 86.1-86.1S429 0 381.5 0s-86.1 38.6-86.1 86.1c0 10 1.7 19.6 4.9 28.5l-132.1 73.8c-15.7-20.6-40.5-33.8-68.3-33.8-47.4 0-86.1 38.6-86.1 86.1s38.7 86.1 86.2 86.1c27.8 0 52.6-13.3 68.4-33.9l132.2 73.9c-3.2 9-5 18.7-5 28.7 0 47.4 38.6 86.1 86.1 86.1s86.1-38.6 86.1-86.1-38.7-86.1-86.2-86.1zm0-282.3c32.6 0 59.1 26.5 59.1 59.1s-26.5 59.1-59.1 59.1-59.1-26.5-59.1-59.1 26.6-59.1 59.1-59.1zM100 299.8c-32.6 0-59.1-26.5-59.1-59.1s26.5-59.1 59.1-59.1 59.1 26.5 59.1 59.1-26.6 59.1-59.1 59.1zm281.6 154.7c-32.6 0-59.1-26.5-59.1-59.1s26.5-59.1 59.1-59.1 59.1 26.5 59.1 59.1-26.5 59.1-59.1 59.1z'/>
                       </svg>
-                    </button>
+                      <span style={styles.details.buttons.shareText}>&nbsp;Share</span>
+                    </a>
                   </div>
                 </div>
               </div>
