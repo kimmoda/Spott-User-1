779dbe24b4236ae2a768a2d52cda8663c65af98f
funnywebdev <alexrunner82@gmail.com>
2016-07-20 09:42:36 +0200

    Background explore what you watch

--- src/pages/home/view/downloadApp/index.js
+++ src/pages/home/view/downloadApp/index.js
@@ -41,7 +41,7 @@ export default class DownloadApp extends Component {
       left: 0,
       bottom: 0,
       right: 0,
-      backgroundImage: 'linear-gradient(to bottom, rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.8))',
+      backgroundImage: 'linear-gradient(rgba(220, 220, 220, 0.8), rgba(255, 255, 255, 0.95))',
       // backgroundImage: 'linear-gradient(to bottom, #000000, rgb(255, 255, 255))',
       pointerEvents: 'none' // Don't capture pointer events. "Click through..."
     },
