054a363e45629fcdad28e211e3a80cbaf339633e
funnywebdev <alexrunner82@gmail.com>
2017-01-16 16:00:10 +0700

    UB token in progress

--- src/api/ub.js
+++ src/api/ub.js
@@ -1,20 +1,39 @@
-import * as request from './requestUB';
+import * as requestUb from './requestUB';
+import * as request from './request';
 import { transformUbProduct } from './transformers';
 
 export async function crawlProduct (baseUrl, authenticationToken, { productUrl }) {
   try {
-    const { body } = await request.post(null, `${baseUrl}/products/crawl`, { url: productUrl, country: 'BE', wait: true });
+    const { body } = await requestUb.post(null, `${baseUrl}/products/crawl`, { url: productUrl, country: 'BE', wait: true });
     return transformUbProduct({ body, productUrl });
   } catch (error) {
     throw '_common.unknown';
   }
 }
 
-export async function getToken (baseUrl, authenticationToken) {
+export async function getNewUbToken (baseUrl, authenticationToken) {
   try {
-    const { access_token: { token } } = await request.post(authenticationToken, `${baseUrl}/oauth/issue`, null);
-    console.log(token);
+    const { access_token: { token } } = await requestUb.post(authenticationToken, `${baseUrl}/oauth/issue`, null);
     return token;
+  } catch (error) {
+    throw '_common.unknown';
+  }
+}
+
+export async function geUbToken (baseUrl, authenticationToken, { userId }) {
+  try {
+    const { body: { token } } = await request.get(authenticationToken, `${baseUrl}/v003/user/users/${userId}/universalBasketAccessToken`);
+    return token;
+  } catch (error) {
+    console.log(error);
+    throw '_common.unknown';
+  }
+}
+
+export async function setUbToken (baseUrl, authenticationToken, { userId, ubToken }) {
+  try {
+    const result = await request.post(authenticationToken, `${baseUrl}/v003/user/users/${userId}/universalBasketAccessToken`, { token: ubToken });
+    return result;
   } catch (error) {
     throw '_common.unknown';
   }
@@ -22,7 +41,7 @@ export async function getToken (baseUrl, authenticationToken) {
 
 export async function addProductToBasket (baseUrl, authenticationToken, { productId }) {
   try {
-    const { body } = await request.post(authenticationToken, `${baseUrl}/basket/add/${productId}`, { affiliateUrl: 'https://spott.it/' });
+    const { body } = await requestUb.post(authenticationToken, `${baseUrl}/basket/add/${productId}`, { affiliateUrl: 'https://spott.it/' });
     return body;
   } catch (error) {
     throw '_common.unknown';
@@ -31,7 +50,7 @@ export async function addProductToBasket (baseUrl, authenticationToken, { produc
 
 export async function removeProductFromBasket (baseUrl, authenticationToken, { lineId }) {
   try {
-    const { body } = await request.del(authenticationToken, `${baseUrl}/basket/remove/${lineId}`);
+    const { body } = await requestUb.del(authenticationToken, `${baseUrl}/basket/remove/${lineId}`);
     return body;
   } catch (error) {
     throw '_common.unknown';
@@ -40,7 +59,7 @@ export async function removeProductFromBasket (baseUrl, authenticationToken, { l
 
 export async function loadBasket (baseUrl, authenticationToken) {
   try {
-    const { body } = await request.get(authenticationToken, `${baseUrl}/basket/get`);
+    const { body } = await requestUb.get(authenticationToken, `${baseUrl}/basket/get`);
     return body;
   } catch (error) {
     throw '_common.unknown';
--- src/pages/basket/actions.js
+++ src/pages/basket/actions.js
@@ -16,6 +16,18 @@ export const LOAD_BASKET_START = 'BASKET/LOAD_BASKET_START';
 export const LOAD_BASKET_SUCCESS = 'BASKET/LOAD_BASKET_SUCCESS';
 export const LOAD_BASKET_ERROR = 'BASKET/LOAD_BASKET_ERROR';
 
+export const GET_UB_TOKEN_START = 'BASKET/GET_UB_TOKEN_START';
+export const GET_UB_TOKEN_SUCCESS = 'BASKET/GET_UB_TOKEN_SUCCESS';
+export const GET_UB_TOKEN_ERROR = 'BASKET/GET_UB_TOKEN_ERROR';
+
+export const LOAD_UB_TOKEN_START = 'BASKET/LOAD_UB_TOKEN_START';
+export const LOAD_UB_TOKEN_SUCCESS = 'BASKET/LOAD_UB_TOKEN_SUCCESS';
+export const LOAD_UB_TOKEN_ERROR = 'BASKET/LOAD_UB_TOKEN_ERROR';
+
+export const SET_UB_TOKEN_START = 'BASKET/SET_UB_TOKEN_START';
+export const SET_UB_TOKEN_SUCCESS = 'BASKET/SET_UB_TOKEN_SUCCESS';
+export const SET_UB_TOKEN_ERROR = 'BASKET/SET_UB_TOKEN_ERROR';
+
 // Actions creators
 // ////////////////
 
@@ -24,3 +36,24 @@ export const addToBasket = makeUbApiActionCreator(ubApi.addProductToBasket, ADD_
 export const removeFromBasket = makeUbApiActionCreator(ubApi.removeProductFromBasket, REMOVE_PRODUCT_START, REMOVE_PRODUCT_SUCCESS, REMOVE_PRODUCT_ERROR);
 
 export const loadBasketData = makeUbApiActionCreator(ubApi.loadBasket, LOAD_BASKET_START, LOAD_BASKET_SUCCESS, LOAD_BASKET_ERROR);
+
+export const loadNewUbToken = makeUbApiActionCreator(ubApi.getNewUbToken, GET_UB_TOKEN_START, GET_UB_TOKEN_SUCCESS, GET_UB_TOKEN_ERROR);
+
+export const loadUbToken = makeUbApiActionCreator(ubApi.geUbToken, LOAD_UB_TOKEN_START, LOAD_UB_TOKEN_SUCCESS, LOAD_UB_TOKEN_ERROR);
+
+export const setUbToken = makeUbApiActionCreator(ubApi.setUbToken, SET_UB_TOKEN_START, SET_UB_TOKEN_SUCCESS, SET_UB_TOKEN_ERROR);
+
+// @TODO in progress
+export async function initUbToken (userId) {
+  return async (dispatch, getState) => {
+    try {
+      const ubToken = await loadUbToken({ userId });
+      if (!ubToken) {
+        const newUbToken = await dispatch(loadNewUbToken());
+        return await dispatch(setUbToken({ userId, ubToken: newUbToken }));
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+}
