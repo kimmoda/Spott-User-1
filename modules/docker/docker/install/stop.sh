054fc204e459d1f32ab98aa6f09e8a87935acebe
funnywebdev <alexrunner82@gmail.com>
2017-01-17 07:47:18 +0100

    seoserver integration use “configuration” container to expose config files

--- modules/docker/docker/install/stop.sh
+++ modules/docker/docker/install/stop.sh
@@ -8,7 +8,7 @@ cd $SCRIPT_DIR
 
 # Start with sample
 if [ ! -f docker-compose.yml ]; then
-    cp docker-compose-sample.yml docker-compose.yml
+	cp docker-compose-sample.yml docker-compose.yml
 fi
 
 # Set local environment
@@ -24,19 +24,7 @@ if [ -z "$DEPLOY_ENV" ]; then
     exit 1
 fi
 
-# We need the configuration directory
-if [ ! -f "$SCRIPT_DIR/config/Dockerfile" ]; then
-    echo "Error: $SCRIPT_DIR/config/Dockerfile does not exist. Unable to create the configuration container"
-    exit 1
-fi
-
-# Create the configuration container
-docker build -t docker.appiness.mobi/spott-$DEPLOY_ENV-website-config:latest $SCRIPT_DIR/config
-docker push docker.appiness.mobi/spott-$DEPLOY_ENV-website-config:latest
-
-# Restart
+# Stop
 docker-compose down -v
-docker-compose pull
-docker-compose up -d --force-recreate
 
 cd $CWD
