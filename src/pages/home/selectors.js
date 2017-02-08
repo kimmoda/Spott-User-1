f85ade78ec5f7674120cad3a17b5ace87856f424
funnywebdev <alexrunner82@gmail.com>
2017-02-08 11:13:26 +0100

    Sharing headers

--- src/pages/home/selectors.js
+++ src/pages/home/selectors.js
@@ -10,36 +10,94 @@ import { isAuthenticatedSelector } from '../app/selector';
 
 const videosById = {
   'trailer-1': {
-    duration: '01:44',
-    fingerprintId: '3203417FFEC27DD3', // TODO: now it's family
+    description: {
+      en: 'Fifty Shades Darker Trailer #1',
+      fr: 'Fifty Shades Darker Trailer #1',
+      nl: 'Fifty Shades Darker Trailer #1'
+    },
+    duration: '01:44', // TODO
+    fingerprintId: '855EB3628BD33309',
     id: 'trailer-1',
     poster: require('./view/recentlyAdded/images/trailer-1-poster.png'),
     thumb: require('./view/recentlyAdded/images/trailer-1-thumb.png'),
+    title: {
+      en: 'Fifty Shades Darker Trailer #1',
+      fr: 'Fifty Shades Darker Trailer #1',
+      nl: 'Fifty Shades Darker Trailer #1'
+    },
+    videoUrl: {
+      en: 'https://appinessmedia.blob.core.windows.net/spott/50_dark_01_en/1080p/index.m3u8',
+      fr: 'https://appinessmedia.blob.core.windows.net/spott/50_dark_01_fr/1080p/index.m3u8',
+      nl: 'https://appinessmedia.blob.core.windows.net/spott/50_dark_01_nl/1080p/index.m3u8'
+    }
+  },
+  'trailer-2': {
+    description: {
+      en: 'Fifty Shades Darker Trailer #2',
+      fr: 'Fifty Shades Darker Trailer #2',
+      nl: 'Fifty Shades Darker Trailer #2'
+    },
+    duration: '01:44', // TODO
+    fingerprintId: '34BEED95DACBD5D5',
+    id: 'trailer-2',
+    poster: require('./view/recentlyAdded/images/trailer-1-poster.png'),
+    thumb: require('./view/recentlyAdded/images/trailer-1-thumb.png'),
+    title: {
+      en: 'Fifty Shades Darker Trailer #2',
+      fr: 'Fifty Shades Darker Trailer #2',
+      nl: 'Fifty Shades Darker Trailer #2'
+    },
+    videoUrl: {
+      en: 'https://appinessmedia.blob.core.windows.net/spott/50_dark_02_en/1080p/index.m3u8',
+      fr: 'https://appinessmedia.blob.core.windows.net/spott/50_dark_02_fr/1080p/index.m3u8',
+      nl: 'https://appinessmedia.blob.core.windows.net/spott/50_dark_02_nl/1080p/index.m3u8'
+    }
+  },
+  'trailer-3': {
+    description: {
+      en: 'Fifty Shades Of Grey Trailer #2',
+      fr: 'Fifty Shades Of Grey Trailer #2',
+      nl: 'Fifty Shades Of Grey Trailer #2'
+    },
+    duration: '01:44', // TODO
+    fingerprintId: 'E56F4B707FD38E15',
+    id: 'trailer-3',
+    poster: require('./view/recentlyAdded/images/trailer-1-poster.png'),
+    thumb: require('./view/recentlyAdded/images/trailer-1-thumb.png'),
+    title: {
+      en: 'Fifty Shades Of Grey Trailer #2',
+      fr: 'Fifty Shades Of Grey Trailer #2',
+      nl: 'Fifty Shades Of Grey Trailer #2'
+    },
     videoUrl: {
       en: 'https://appinessmedia.blob.core.windows.net/spott/50_grey_01_en/1080p/index.m3u8',
-      fr: 'https://appinessmedia.blob.core.windows.net/spott/50_grey_01_en/1080p/index.m3u8',
-      nl: 'https://appinessmedia.blob.core.windows.net/spott/50_grey_01_en/1080p/index.m3u8'
+      fr: 'https://appinessmedia.blob.core.windows.net/spott/50_grey_01_fr/1080p/index.m3u8',
+      nl: 'https://appinessmedia.blob.core.windows.net/spott/50_grey_01_nl/1080p/index.m3u8'
+    }
+  },
+  'trailer-4': {
+    description: {
+      en: 'Fifty Shades Of Grey Trailer #2',
+      fr: 'Fifty Shades Of Grey Trailer #2',
+      nl: 'Fifty Shades Of Grey Trailer #2'
     },
+    duration: '01:44', // TODO
+    fingerprintId: 'E3221922A61BAC2A',
+    id: 'trailer-4',
+    poster: require('./view/recentlyAdded/images/trailer-1-poster.png'),
+    thumb: require('./view/recentlyAdded/images/trailer-1-thumb.png'),
     title: {
-      en: 'Fifty Shades Of Grey Trailer #1',
-      fr: 'Fifty Shades Of Grey Trailer #1',
-      nl: 'Fifty Shades Of Grey Trailer #1'
+      en: 'Fifty Shades Of Grey Trailer #2',
+      fr: 'Fifty Shades Of Grey Trailer #2',
+      nl: 'Fifty Shades Of Grey Trailer #2'
+    },
+    videoUrl: {
+      en: 'https://appinessmedia.blob.core.windows.net/spott/50_grey_02_en/1080p/index.m3u8',
+      fr: 'https://appinessmedia.blob.core.windows.net/spott/50_grey_02_fr/1080p/index.m3u8',
+      nl: 'https://appinessmedia.blob.core.windows.net/spott/50_grey_02_nl/1080p/index.m3u8'
     }
   }
 };
-
-// Generate 4 videos
-for (let i = 2; i <= 4; i++) {
-  videosById[`trailer-${i}`] = {
-    ...videosById['trailer-1'],
-    id: `trailer-${i}`,
-    title: {
-      en: `Fifty Shades Of Grey Trailer #${i}`,
-      fr: `Fifty Shades Of Grey Trailer #${i}`,
-      nl: `Fifty Shades Of Grey Trailer #${i}`
-    }
-  };
-}
 
 const playlist = [];
 for (let i = 1; i <= 4; i++) {
--- src/pages/home/view/recentlyAdded/index.js
+++ src/pages/home/view/recentlyAdded/index.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import Radium from 'radium';
 import { connect } from 'react-redux';
+import Helmet from 'react-helmet';
 import { colors, fontWeights, makeTextStyle, mediaQueries, pinkButtonStyle, responsiveBackgroundImage, Button, Container, Title, UpperCaseSubtitle } from '../../../_common/buildingBlocks';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import TopLevelMediumTiles from '../../../_common/tiles/topLevelMediumTiles';
@@ -9,12 +10,40 @@ import { recentlyAddedSelector } from '../../selectors';
 import Video from './video';
 import Playlist from './playlist';
 
+class SharingHeaders extends Component {
+  static propTypes = {
+    currentLocale: PropTypes.string.isRequired,
+    video: PropTypes.object.isRequired
+  }
+
+  render () {
+    const { currentLocale, video } = this.props;
+    const meta = [
+      { property: 'fb:app_id', content: '418487828343937' },
+      { property: 'og:description', content: video.description[currentLocale] },
+      { property: 'og:image', content: video.poster },
+      { property: 'og:site_name', content: 'Spott' },
+      { property: 'og:title', content: `Spott - ${video.title[currentLocale]}` },
+      { property: 'og:type', content: 'video.movie' },
+      { property: 'title', content: `Spott - ${video.title[currentLocale]}` },
+      { property: 'twitter:card', content: `Spott - ${video.title[currentLocale]}` },
+      { property: 'twitter:description', content: video.description[currentLocale] },
+      { property: 'twitter:domain', content: 'https://spott.it' },
+      { property: 'twitter:image', content: video.poster },
+      { property: 'twitter:site', content: '@SpottBE_nl' },
+      { property: 'twitter:title', content: `Spott - ${video.title[currentLocale]}` }
+    ];
+    return <Helmet meta={meta} />;
+  }
+}
+
 @localized
 @connect(recentlyAddedSelector)
 @Radium
 export default class RecentlyAdded extends Component {
 
   static propTypes = {
+    currentLocale: PropTypes.string.isRequired,
     firstMedium: ImmutablePropTypes.mapContains({
       profileImage: ImmutablePropTypes.mapContains({
         url: PropTypes.string.isRequired
@@ -89,7 +118,7 @@ export default class RecentlyAdded extends Component {
 
   render () {
     const { styles } = this.constructor;
-    const { firstMedium, params, playlist, otherRecentlyAddedMedia, style, t, videosById } = this.props;
+    const { currentLocale, firstMedium, params, playlist, otherRecentlyAddedMedia, style, t, videosById } = this.props;
     const videoId = params && params.trailer || 'trailer-1';
     console.warn('params', params);
     return (
@@ -98,6 +127,7 @@ export default class RecentlyAdded extends Component {
         firstMedium && firstMedium.get('profileImage') && responsiveBackgroundImage(firstMedium.getIn([ 'profileImage', 'url' ])),
         style
       ]}>
+        <SharingHeaders currentLocale={currentLocale} video={videosById[videoId]}/>
         <Container>
           <div style={styles.overlay} />
           <div style={{ display: 'flex' }}>
