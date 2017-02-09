d16f2f7418a1083169b6c4a6dd8c4b2fb7b6f53e
funnywebdev <alexrunner82@gmail.com>
2017-02-09 09:55:24 +0100

    No recent episodes for movies, commercials

--- src/pages/medium/view/overview/index.js
+++ src/pages/medium/view/overview/index.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import { COMMERCIAL } from '../../../../data/mediumTypes';
+import { COMMERCIAL, SERIES } from '../../../../data/mediumTypes';
 // import NewScenesForYou from './newScenesForYou';
 import PickedForYou from './pickedForYou';
 import TopProducts from './topProducts';
@@ -31,7 +31,8 @@ export default class Overview extends Component {
         {/* Products for you is hidden for a commercial. */}
         {medium.get('type') !== COMMERCIAL &&
           <PickedForYou mediumId={mediumId} />}
-        <RecentEpisodes location={location} mediumId={mediumId} />
+        {medium.get('type') === SERIES &&
+          <RecentEpisodes location={location} mediumId={mediumId} />}
         <TopProducts location={location} mediumId={mediumId} />
         {/*
         <PeopleAlsoWatch />
