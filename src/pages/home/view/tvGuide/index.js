e6abf900b3d8b673673b304394a039bfa24cb293
funnywebdev <alexrunner82@gmail.com>
2017-03-02 03:38:45 +0500

    SWEWBSITE-335 - Don't show the "Watch TV with Spott" section if there are no TV guide entries

--- src/pages/home/view/tvGuide/index.js
+++ src/pages/home/view/tvGuide/index.js
@@ -1,11 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import Radium from 'radium';
 import { connect } from 'react-redux';
-import { Container, Message } from '../../../_common/buildingBlocks';
+import { Container, Message, colors } from '../../../_common/buildingBlocks';
 import { tvGuideEntriesSelector } from '../../selectors';
 import localized from '../../../_common/localized';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import TvGuideTiles from '../../../_common/tiles/tvGuideTiles';
+import { LOADED, UPDATING } from '../../../../data/statusTypes';
 
 @localized
 @connect(tvGuideEntriesSelector, (dispatch) => ({
@@ -27,12 +28,22 @@ export default class NewEpisodes extends Component {
     wrapper: {
       paddingTop: '2.5em',
       paddingBottom: '3.125em'
+    },
+    empty: {
+      backgroundColor: colors.white,
+      height: '4.5em',
+      marginTop: '-5.5em'
     }
   };
 
   render () {
     const { styles } = this.constructor;
     const { style, tvGuideEntries, t } = this.props;
+    return (<div style={styles.empty} />);
+    if ((tvGuideEntries.get('_status') === LOADED || tvGuideEntries.get('_status') === UPDATING) && (tvGuideEntries.get('data') && tvGuideEntries.get('data').size === 0)) {
+      return (<div style={styles.empty} />);
+    }
+
     return (
       <div style={[ styles.wrapper, style ]}>
         <Container>
