c178c525c6fef440ab9ef8e7751d94255fe052b2
funnywebdev <alexrunner82@gmail.com>
2017-03-25 00:55:47 +0500

    SWEWBSITE-361 - Allow user to close the overlay by clicking on the overlay itself too

--- src/pages/newHome/view/cardModal/index.js
+++ src/pages/newHome/view/cardModal/index.js
@@ -26,6 +26,7 @@ export default class CardModal extends Component {
     super(props);
     this.onProductClick = ::this.onProductClick;
     this.onSidebarClose = ::this.onSidebarClose;
+    this.onWrapperClick = ::this.onWrapperClick;
 
     this.state = {
       sidebarItemIndex: 1,
@@ -68,6 +69,12 @@ export default class CardModal extends Component {
     });
   }
 
+  onWrapperClick (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.props.onClose();
+  }
+
   render () {
     const { onClose } = this.props;
 
@@ -77,69 +84,70 @@ export default class CardModal extends Component {
         isOpen
         overlayClassName={styles['modal-overlay']}
         onRequestClose={onClose}>
-        <div styleName='wrapper'>
-          <div styleName='modal-close' onClick={onClose}>
-            <i><IconClose/></i>
-          </div>
-          <div className={this.state.sidebarItem ? styles['main-sidebar-active'] : styles['main-sidebar-inactive']} styleName='main'>
-            <div styleName='card'>
-              <div styleName='image'>
-                <img src='http://lorempixel.com/592/830/abstract/7'/>
-                <CardMarkers/>
-              </div>
-              <div styleName='products'>
-                {new Array(7).fill(1).map((item, index) =>
-                  <div key={`product_${index}`}
-                       style={{ backgroundImage: `url(http://lorempixel.com/80/80/abstract/${index})` }}
-                       styleName='product'
-                       onClick={this.onProductClick}/>
-                )}
-              </div>
-              <div styleName='content'>
-                <h3 styleName='title'>Erin Lindsay in Chicago Med</h3>
-                <div styleName='description'>
-                  Taken from Season 2 Episode 5 — Extreme Measures
-                </div>
-                <div styleName='topic-links'>
-                  <Link styleName='topic-link' to='#'>Gabriel Macht</Link>
-                  <Link styleName='topic-link' to='#'>Harvey Specter</Link>
-                  <Link styleName='topic-link' to='#'>Red Carpet</Link>
-                  <Link styleName='topic-link' to='#'>Tom Ford</Link>
-                  <Link styleName='topic-link' to='#'>Suits</Link>
-                  <Link styleName='topic-link' to='#'>Series</Link>
-                  <Link styleName='topic-link' to='#'>Suit</Link>
-                </div>
-              </div>
-              <div styleName='footer'>
-                <Link styleName='likes' to='#'>
-                  <i><IconHeart/></i>
-                  <span>24</span>
-                </Link>
-                <div styleName='users'>
-                  <Users large />
-                </div>
-                <Link styleName='moar' to='#'>
-                  <i><IconForward/></i>
-                </Link>
-              </div>
-            </div>
-            <div styleName='topics'>
-              <div styleName='topics-content'>
-                <div styleName='topics-title'>Related Topics</div>
-                <Topics/>
-              </div>
-            </div>
-            <div styleName='spotts'>
-              <div styleName='spotts-title'>Similar Spotts</div>
-              <div styleName='spotts-list'>
-                {new Array(9).fill(1).map((item, index) =>
-                  <Card key={`modal_card_${index}`} />
-                )}
-              </div>
-            </div>
-          </div>
-          <Sidebars item={this.state.sidebarItem} onSidebarClose={this.onSidebarClose} />
+        <div styleName='modal-close-layer' onClick={onClose}/>
+        <div styleName='modal-close' onClick={onClose}>
+          <i><IconClose/></i>
         </div>
+        <div className={this.state.sidebarItem ? styles['main-sidebar-active'] : styles['main-sidebar-inactive']}
+             styleName='main'>
+          <div styleName='modal-close-layer' onClick={onClose}/>
+          <div styleName='card'>
+            <div styleName='image'>
+              <img src='http://lorempixel.com/592/830/abstract/7'/>
+              <CardMarkers/>
+            </div>
+            <div styleName='products'>
+              {new Array(7).fill(1).map((item, index) =>
+                <div key={`product_${index}`}
+                     style={{ backgroundImage: `url(http://lorempixel.com/80/80/abstract/${index})` }}
+                     styleName='product'
+                     onClick={this.onProductClick}/>
+              )}
+            </div>
+            <div styleName='content'>
+              <h3 styleName='title'>Erin Lindsay in Chicago Med</h3>
+              <div styleName='description'>
+                Taken from Season 2 Episode 5 — Extreme Measures
+              </div>
+              <div styleName='topic-links'>
+                <Link styleName='topic-link' to='#'>Gabriel Macht</Link>
+                <Link styleName='topic-link' to='#'>Harvey Specter</Link>
+                <Link styleName='topic-link' to='#'>Red Carpet</Link>
+                <Link styleName='topic-link' to='#'>Tom Ford</Link>
+                <Link styleName='topic-link' to='#'>Suits</Link>
+                <Link styleName='topic-link' to='#'>Series</Link>
+                <Link styleName='topic-link' to='#'>Suit</Link>
+              </div>
+            </div>
+            <div styleName='footer'>
+              <Link styleName='likes' to='#'>
+                <i><IconHeart/></i>
+                <span>24</span>
+              </Link>
+              <div styleName='users'>
+                <Users large/>
+              </div>
+              <Link styleName='moar' to='#'>
+                <i><IconForward/></i>
+              </Link>
+            </div>
+          </div>
+          <div styleName='topics'>
+            <div styleName='topics-content'>
+              <div styleName='topics-title'>Related Topics</div>
+              <Topics/>
+            </div>
+          </div>
+          <div styleName='spotts'>
+            <div styleName='spotts-title'>Similar Spotts</div>
+            <div styleName='spotts-list'>
+              {new Array(9).fill(1).map((item, index) =>
+                <Card key={`modal_card_${index}`}/>
+              )}
+            </div>
+          </div>
+        </div>
+        <Sidebars item={this.state.sidebarItem} onSidebarClose={this.onSidebarClose}/>
       </ReactModal>
     );
   }
