6b86d0167a18ca08d246800619266d74e9f3da46
funnywebdev <alexrunner82@gmail.com>
2017-04-05 15:02:39 +0500

    topic page sticky info block

--- src/pages/newHome/view/topicPage/index.js
+++ src/pages/newHome/view/topicPage/index.js
@@ -5,7 +5,9 @@ import localized from '../../../_common/localized';
 import Topics from '../topics';
 import Cards from '../cards';
 import { IconForward } from '../icons';
+
 const styles = require('./index.scss');
+const { cssHeaderHeight } = require('../vars.scss');
 
 @localized
 @CSSModules(styles, { allowMultiple: true })
@@ -13,30 +15,57 @@ export default class NewTopic extends Component {
 
   constructor (props) {
     super(props);
-
+    this.handleScroll = ::this.handleScroll;
+    this.state = {
+      isScrolledToInfo: false
+    };
     this.infoImage = 'https://spott-ios-rest-prd.appiness.mobi/spott/rest/v003/image/images/5b447383-ed89-40e7-a71a-a19788a00406?height=72&width=48';
+    this.headerHeight = parseInt(cssHeaderHeight, 10);
+    this.infoContainerOffsetTop = null;
+    this.infoContainerHeight = null;
+    this.isScrolledToInfo = false;
+  }
+
+  componentDidMount () {
+    this.infoContainerOffsetTop = this.infoContainer.offsetTop;
+    this.infoContainerHeight = this.infoContainer.clientHeight;
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll () {
+    this.setState({
+      isScrolledToInfo: this.infoContainerOffsetTop <= window.scrollY + this.headerHeight
+    });
   }
 
   render () {
+    const { isScrolledToInfo } = this.state;
+
     return (
       <section styleName='wrapper'>
         <div styleName='poster'/>
-        <div styleName='info'>
-          <div styleName='info-content'>
-            <div styleName='info-left'>
-              <div style={{ backgroundImage: `url('${this.infoImage}')` }} styleName='info-image'/>
-              <div styleName='info-header'>
-                <h2 styleName='info-title'>Suits</h2>
-                <div styleName='info-type'>Tv Series</div>
+        <div style={{ height: this.infoContainerHeight }} styleName='info-wrapper'>
+          <div className={isScrolledToInfo && styles['info-sticky']} ref={(ref) => { this.infoContainer = ref; }} styleName='info'>
+            <div styleName='info-content'>
+              <div styleName='info-left'>
+                <div style={{ backgroundImage: `url('${this.infoImage}')` }} styleName='info-image'/>
+                <div styleName='info-header'>
+                  <h2 styleName='info-title'>Suits</h2>
+                  <div styleName='info-type'>Tv Series</div>
+                </div>
               </div>
-            </div>
-            <div styleName='info-subscribers'>
-              <div styleName='info-subscribers-count'>856</div>
-              <div styleName='info-subscribers-text'>Subscribers</div>
-            </div>
-            <div styleName='info-subscribe-btn'>Subscribed</div>
-            <div styleName='info-share'>
-              <i><IconForward/></i>
+              <div styleName='info-subscribers'>
+                <div styleName='info-subscribers-count'>856</div>
+                <div styleName='info-subscribers-text'>Subscribers</div>
+              </div>
+              <div styleName='info-subscribe-btn'>Subscribed</div>
+              <div styleName='info-share'>
+                <i><IconForward/></i>
+              </div>
             </div>
           </div>
         </div>
