8ed771b3a573fcc8b8238fc919b5776ffe87dd3b
funnywebdev <alexrunner82@gmail.com>
2017-07-27 04:24:47 +0300

    SWEWBSITE-564 - Add Portuguese language to Spott

--- src/lang/index.js
+++ src/lang/index.js
@@ -2,23 +2,27 @@ export default {
   localeData: {
     en: require('./en.json'),
     fr: require('./fr.json'),
-    nl: require('./nl.json')
+    nl: require('./nl.json'),
+    pt: require('./pt.json')
   },
   localizedResources: {
     cookies: {
       en: require('./en.cookies.html'),
       fr: require('./fr.cookies.html'),
-      nl: require('./nl.cookies.html')
+      nl: require('./nl.cookies.html'),
+      pt: require('./en.cookies.html')
     },
     privacy: {
       en: require('./en.privacy.html'),
       fr: require('./fr.privacy.html'),
-      nl: require('./nl.privacy.html')
+      nl: require('./nl.privacy.html'),
+      pt: require('./en.privacy.html')
     },
     terms: {
       en: require('./en.terms.html'),
       fr: require('./fr.terms.html'),
-      nl: require('./nl.terms.html')
+      nl: require('./nl.terms.html'),
+      pt: require('./en.terms.html')
     }
   }
 };
