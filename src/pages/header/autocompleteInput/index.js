e4a0fb8deabac16f295590c82deb8f93660fe4c3
funnywebdev <alexrunner82@gmail.com>
2017-08-22 19:12:54 +0500

    SWEWBSITE-550 - Search isn’t center aligned

--- src/pages/header/autocompleteInput/index.js
+++ src/pages/header/autocompleteInput/index.js
@@ -2,12 +2,15 @@
 import React, { Component, PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import { IconSearch, IconClose } from '../../icons';
+import localized from '../../_common/localized';
 
 const styles = require('../index.scss');
 
+@localized
 @CSSModules(styles, { allowMultiple: true })
 class AutocompleteInput extends Component {
   static propTypes = {
+    currentLocale: PropTypes.string.isRequired,
     placeholder: PropTypes.any,
     value: PropTypes.any,
     onBlur: PropTypes.func,
@@ -39,13 +42,21 @@ class AutocompleteInput extends Component {
   }
 
   render () {
-    const { placeholder, value, onChange, onKeyDown } = this.props;
+    const { placeholder, value, onChange, onKeyDown, currentLocale } = this.props;
+    let inputSize = 14;
+    if (currentLocale === 'fr') {
+      inputSize = 18;
+    }
+    if (currentLocale === 'pt') {
+      inputSize = 20;
+    }
     return (
       <div styleName='search-input-wrapper' tabIndex='0' onBlur={this.onBlur} onFocus={this.onFocus}>
         <i><IconSearch/></i>
         <input
           placeholder={placeholder}
           ref={(input) => { this.textInput = input; }}
+          size={inputSize}
           styleName='search-input'
           value={value}
           onChange={onChange}
